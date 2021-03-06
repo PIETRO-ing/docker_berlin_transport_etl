5e5f4a7673508d673812e1f8b2bbd69f5843bba3
PIETRO-ing <pa.pietro81@gmail.com>
2021-03-06 03:01:15 +0100

    berlin transport api to mongodb to postgresql

---
+++ etl_job/etl.py
@@ -0,0 +1,79 @@
+# 1. extract data from MongoDB
+# - in which frequency do we extract data from the MongoDB?
+# - when do we extract the data?
+# - are we extracting the whole data or a subset of the data?
+# - in which order are we extracting data?
+# - how do we handle error?
+# - what information do we log?
+# - extract all of the data or just new data? - full vs. incremental load 
+
+
+
+# 2. apply some sentiment analysis (Transform)
+# - what format is the data in?
+# - what format should it be transformed to?
+# - what calculations/aggregations do i want to perform?
+# - error handling
+# - logging
+
+
+# 3. load it to Postgres
+# - scheduling
+# - error handling
+# - logging
+
+# --------------------------------------
+
+import time
+import pymongo
+import logging
+
+
+logging.critical("Hello World")
+
+while True:
+
+    time.sleep(10)  # seconds
+
+    # Establish a connection to the MongoDB server
+    client = pymongo.MongoClient("mongodb")
+
+    # Select the database you want to use withing the MongoDB server
+    db = client.departures
+
+    # Select the collection of documents you want to use withing the MongoDB database
+    collection = db.departures_table
+
+
+    # connect to postgres
+    from sqlalchemy import create_engine
+
+
+
+    pg = create_engine('postgres://postgres:postgres@postgresdb:5432/departuresPG', echo=True)
+    pg.execute('''
+        CREATE TABLE IF NOT EXISTS departures_table (
+        departures_from varchar(70),
+        direction varchar(70),
+        delay NUMERIC,
+        time_schedule timestamptz
+
+    );
+    ''')
+
+    # print all entries
+    entries = collection.find()
+    for e in entries:
+    
+        departures_from = e['departures_from']
+        direction = e['direction']
+        delay = e['delay']
+        time_schedule = e['time_schedule']
+    
+        query = "INSERT INTO departures_table VALUES (%s, %s, %s, %s);"
+        pg.execute(query, (departures_from, direction, delay, time_schedule))
+    
+        logging.critical(e)
+    time.sleep(10)
+
+    
